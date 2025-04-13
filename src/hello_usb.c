67f8d781bc948e97b1b434466ff0aee6bf094d7d
lautisilber <lautisilbergleit@gmail.com>
2025-04-13 16:17:43 -0300

    figured how to fix 3.5 cmake compatibility build error

--- src/hello_usb.c
+++ src/hello_usb.c
@@ -4,13 +4,13 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
- #include <stdio.h>
- #include "pico/stdlib.h"
- 
- int main() {
-     stdio_init_all();
-     while (true) {
-         printf("Hello, world!\n");
-         sleep_ms(1000);
-     }
- }
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
